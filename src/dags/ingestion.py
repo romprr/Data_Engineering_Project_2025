32a18affc66a7fe98f3177ae6c89bc89c5957879
Adrian <adriansaleh061@gmail.com>
2025-12-18 20:52:49 +0100

    file cleaning + metadata insertion into redis

--- src/dags/ingestion.py
+++ src/dags/ingestion.py
@@ -20,26 +20,24 @@ logging.getLogger('yfinance').setLevel(logging.CRITICAL)
 # =========================
 # ENV VARIABLES IMPORTS
 # ========================= 
-INDICES_SYMBOLS_SCRAPER_URL=os.getenv("INDICES_SYMBOLS_SCRAPER_URL")
-FUTURES_SYMBOLS_SCRAPER_URL=os.getenv("FUTURES_SYMBOLS_SCRAPER_URL")
-FOREX_SYMBOLS_SCRAPER_URL=os.getenv("FOREX_SYMBOLS_SCRAPER_URL")
-WORLDWIDE_EVENTS_CSV_FILE_URL=os.getenv("WORLDWIDE_EVENTS_CSV_FILE_URL")
-SHARED_FOLDER_PATH_AIRFLOW=os.getenv("SHARED_FOLDER_PATH_AIRFLOW")
-MONGO_DB_INGESTION_COLLECTION=os.getenv("MONGO_DB_INGESTION_COLLECTION")
-MONGO_DB_INGESTION_DATABASE=os.getenv("MONGO_DB_INGESTION_DATABASE")
-MONGO_DB_URI=os.getenv("MONGO_DB_URI")
-REDIS_1_URI=os.getenv("REDIS_1_URI")
-
-# =========================
-# GLOBAL VARIABLES 
-# =========================
-ASSETS_TYPES = ["forex", "futures", "indices", "crypto"]
-ASSETS_SCRAPPER_URLS = [
-    FOREX_SYMBOLS_SCRAPER_URL,
-    FUTURES_SYMBOLS_SCRAPER_URL,
-    INDICES_SYMBOLS_SCRAPER_URL,
-    None  # Crypto symbols are hardcoded, no scrapper URL
-]
+INDICES_SYMBOLS_SCRAPER_URL = os.getenv("INDICES_SYMBOLS_SCRAPER_URL")
+FUTURES_SYMBOLS_SCRAPER_URL = os.getenv("FUTURES_SYMBOLS_SCRAPER_URL")
+FOREX_SYMBOLS_SCRAPER_URL = os.getenv("FOREX_SYMBOLS_SCRAPER_URL")
+WORLDWIDE_EVENTS_CSV_FILE_URL = os.getenv("WORLDWIDE_EVENTS_CSV_FILE_URL")
+SHARED_FOLDER_PATH_AIRFLOW = os.getenv("SHARED_FOLDER_PATH_AIRFLOW")
+MONGO_DB_INGESTION_COLLECTION = os.getenv("MONGO_DB_INGESTION_COLLECTION")
+MONGO_DB_INGESTION_DATABASE = os.getenv("MONGO_DB_INGESTION_DATABASE")
+MONGO_DB_URI = os.getenv("MONGO_DB_URI")
+REDIS_1_URI = os.getenv("REDIS_1_URI")
+CRYPTO_INFO_QUEUE = os.getenv("CRYPTO_INFO_QUEUE")
+FOREX_INFO_QUEUE = os.getenv("FOREX_INFO_QUEUE")
+FUTURES_INFO_QUEUE = os.getenv("FUTURES_INFO_QUEUE")
+INDICES_INFO_QUEUE = os.getenv("INDICES_INFO_QUEUE")
+CRYPTO_HISTORY_QUEUE = os.getenv("CRYPTO_HISTORY_QUEUE")
+FOREX_HISTORY_QUEUE = os.getenv("FOREX_HISTORY_QUEUE")
+FUTURES_HISTORY_QUEUE = os.getenv("FUTURES_HISTORY_QUEUE")
+INDICES_HISTORY_QUEUE = os.getenv("INDICES_HISTORY_QUEUE")
+FILES_QUEUE = os.getenv("FILES_QUEUE")
 
 # =========================
 # DAG DEFINITION
@@ -61,8 +59,10 @@ default_args = {
     catchup=False, 
     tags=["ingestion"]
     )
+
 def ingestion_pipeline():
     """Ingestion DAG to extract data from various sources and load into MongoDB"""
+
     @task 
     def start():
         """Empty start task"""
@@ -75,11 +75,6 @@ def ingestion_pipeline():
             symbols[i : i + chunk_size]
             for i in range(0, len(symbols), chunk_size)
         ]
-    
-    @task
-    def end():
-        """Empty end task"""
-        print("Ingestion pipeline completed")
 
     @task
     def get_asset_symbols(asset_type, URL):
@@ -102,7 +97,7 @@ def ingestion_pipeline():
     def get_asset_info(asset_type, symbols):
         """Get asset info from yfinance and store in MongoDB. 
         The MongoDB document ID is formatted as {asset_type}_{symbol}_info 
-        to be later pushed into redis"""
+        to be later pushed into redis with other metadata."""
         print(f"Fetching info for asset type: {asset_type}")
         mongoClient = mongo.MongoDBClient(
             uri=MONGO_DB_URI,
@@ -110,67 +105,73 @@ def ingestion_pipeline():
             collection=MONGO_DB_INGESTION_COLLECTION
         )
         mongoClient.connect()
-        ids = []
+        metadatas = []
         for symbol in symbols : 
             print(f"Fetching info for symbol: {symbol}")
             ticker = yf.Ticker(symbol)
             info = ticker.info
             info["symbol"] = symbol
             id = asset_type + "_" + symbol + "_info"
-            ids.append(id)
-            doc = {
+            metadata = {
                 "_id" : id,
                 "type": asset_type,
+                "extracted_at": datetime.now().isoformat(),                
+            }
+            metadatas.append(metadata)
+            doc = {
+                **metadata,
                 "data": info,
-                "extracted_at": datetime.now(),                
             }
             mongoClient.write(doc)
         mongoClient.disconnect()
         print(f"Completed fetching info for asset type: {asset_type}")
-        return ids
+        return metadatas
 
     @task
     def get_asset_history(asset_type, symbols):
         """Get asset historical data from yfinance and store in MongoDB. 
         The MongoDB document ID is formatted as {asset_type}_{symbol}_history
-        to be later pushed into redis"""
+        to be later pushed into redis with other metadata."""
         print(f"Fetching history for asset type: {asset_type}")
         mongoClient = mongo.MongoDBClient(
             uri=MONGO_DB_URI,
-            database="raw_data_db",
-            collection="ingestion"
+            database=MONGO_DB_INGESTION_DATABASE,
+            collection=MONGO_DB_INGESTION_COLLECTION
         )
         mongoClient.connect()
-        ids = []
+        metadatas = []
         for symbol in symbols : 
             ticker = yf.Ticker(symbol)
             hist = ticker.history(period=f"{20*12}mo", interval="1mo") 
             hist = hist.reset_index()
             hist = json.loads(hist.to_json(orient="records", date_format="epoch", date_unit="s"))
             id = asset_type + "_" + symbol + "_history"
-            ids.append(id)
-            doc = {
+            metadata = {
                 "_id" : id,
                 "type": asset_type,
+                "extracted_at": datetime.now().isoformat(),
+            }
+            metadatas.append(metadata)
+            doc = {
+                **metadata,
                 "data": hist,
-                "extracted_at": datetime.now(),
             }
             mongoClient.write(doc)
         mongoClient.disconnect()
         print(f"Completed fetching history for asset type: {asset_type}")
-        return ids
+        return metadatas
     
     @task 
     def populate_redis_queue(data, queue_name):
-        """Pushing mongo document IDs into Redis queue for further processing."""
+        """Pushing mongo document metadata into Redis queue for further processing."""
         print(f"Pushing data into Redis queue: {queue_name}")
         redisClient = redis.RedisClient(uri=REDIS_1_URI)
         redisClient.connect()
         if isinstance(data, (list, tuple, set)):
-            for id in data:
-                redisClient.write(queue_name, id)
+            for metadata in data:
+                redisClient.write(queue_name, json.dumps(metadata))
         else:
-            redisClient.write(queue_name, data)
+            redisClient.write(queue_name, json.dumps(data))
         redisClient.disconnect()
         print(f"Completed pushing data into Redis queue: {queue_name}")
 
@@ -197,7 +198,7 @@ def ingestion_pipeline():
         return file_path
 
     @task 
-    def unzip_file(zip_file_path, extract_to_path):
+    def unzip_file(data_type, zip_file_path, extract_to_path):
         """Unzip a zip file to a specified directory and delete the zip file after extraction."""
         print(f"Unzipping file {zip_file_path} to {extract_to_path}...")
         with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
@@ -214,60 +215,59 @@ def ingestion_pipeline():
         if extracted_files:
             abs_path = os.path.abspath(os.path.join(extract_to_path, extracted_files[0]))
             print(f"Extracted file absolute path: {abs_path}")
-            return abs_path
+            return {
+                "type" : data_type,
+                "data" : abs_path, 
+                "extracted_at": datetime.now().isoformat(),
+            }
         else:
             return None
     
-
-    # ==========================
-    # SYMBOLS EXTRACTION TASKS
-    # ==========================
+    @task
+    def end():
+        """Empty end task"""
+        print("Ingestion pipeline completed")
+    
+    # SYMBOLS EXTRACTION
     crypto_symbols = get_asset_symbols("crypto", URL=None)
     forex_symbols = get_asset_symbols("forex", URL=FOREX_SYMBOLS_SCRAPER_URL)
     futures_symbols = get_asset_symbols("futures", URL=FUTURES_SYMBOLS_SCRAPER_URL)
     indices_symbols = get_asset_symbols("indices", URL=INDICES_SYMBOLS_SCRAPER_URL)
 
-    # chunking
+    # CHUNKING SYMBOLS FOR PARALLEL PROCESSING
     crypto_chunks = chunk_list(crypto_symbols, 5)
     forex_chunks = chunk_list(forex_symbols, 5)
     futures_chunks = chunk_list(futures_symbols, 5)
     indices_chunks = chunk_list(indices_symbols, 5)
 
-    end_task = end()
+    # ASSET INFO AND HISTORY EXTRACTION
+    crypto_info_metadata = get_asset_info.partial(asset_type="crypto").expand(symbols=crypto_chunks)
+    forex_info_metadata = get_asset_info.partial(asset_type="forex").expand(symbols=forex_chunks)
+    futures_info_metadata = get_asset_info.partial(asset_type="futures").expand(symbols=futures_chunks)
+    indices_info_metadata = get_asset_info.partial(asset_type="indices").expand(symbols=indices_chunks)
 
-    # ==========================
-    # INFO EXTRACTION TASKS
-    # ==========================
-    crypto_info_keys = get_asset_info.partial(asset_type="crypto").expand(symbols=crypto_chunks)
-    forex_info_keys = get_asset_info.partial(asset_type="forex").expand(symbols=forex_chunks)
-    futures_info_keys = get_asset_info.partial(asset_type="futures").expand(symbols=futures_chunks)
-    indices_info_keys = get_asset_info.partial(asset_type="indices").expand(symbols=indices_chunks)
-    populate_redis_queue.partial(queue_name="crypto_info_queue").expand(data=crypto_info_keys) >> end_task
-    populate_redis_queue.partial(queue_name="forex_info_queue").expand(data=forex_info_keys) >> end_task
-    populate_redis_queue.partial(queue_name="futures_info_queue").expand(data=futures_info_keys) >> end_task
-    populate_redis_queue.partial(queue_name="indices_info_queue").expand(data=indices_info_keys) >> end_task    
+    crypto_history_metadata = get_asset_history.partial(asset_type="crypto").expand(symbols=crypto_chunks)
+    forex_history_metadata = get_asset_history.partial(asset_type="forex").expand(symbols=forex_chunks)
+    futures_history_metadata = get_asset_history.partial(asset_type="futures").expand(symbols=futures_chunks)
+    indices_history_metadata = get_asset_history.partial(asset_type="indices").expand(symbols=indices_chunks)
 
-    crypto_history_keys = get_asset_history.partial(asset_type="crypto").expand(symbols=crypto_chunks)
-    forex_history_keys = get_asset_history.partial(asset_type="forex").expand(symbols=forex_chunks)
-    futures_history_keys = get_asset_history.partial(asset_type="futures").expand(symbols=futures_chunks)
-    indices_history_keys = get_asset_history.partial(asset_type="indices").expand(symbols=indices_chunks)
+    # CSV FILE DOWNLOAD AND EXTRACTION
+    file_path = download_file(URL=WORLDWIDE_EVENTS_CSV_FILE_URL, path=SHARED_FOLDER_PATH_AIRFLOW)
+    extracted_metadata = unzip_file(data_type="worldwide_events", zip_file_path=file_path, extract_to_path=SHARED_FOLDER_PATH_AIRFLOW)
 
-    populate_redis_queue.partial(queue_name="crypto_history_queue").expand(data=crypto_history_keys) >> end_task
-    populate_redis_queue.partial(queue_name="forex_history_queue").expand(data=forex_history_keys) >> end_task
-    populate_redis_queue.partial(queue_name="futures_history_queue").expand(data=futures_history_keys) >> end_task
-    populate_redis_queue.partial(queue_name="indices_history_queue").expand(data=indices_history_keys) >> end_task
+    populate_tasks = [
+        populate_redis_queue.partial(queue_name=CRYPTO_INFO_QUEUE).expand(data=crypto_info_metadata),
+        populate_redis_queue.partial(queue_name=FOREX_INFO_QUEUE).expand(data=forex_info_metadata),
+        populate_redis_queue.partial(queue_name=FUTURES_INFO_QUEUE).expand(data=futures_info_metadata),
+        populate_redis_queue.partial(queue_name=INDICES_INFO_QUEUE).expand(data=indices_info_metadata),
+        populate_redis_queue.partial(queue_name=CRYPTO_HISTORY_QUEUE).expand(data=crypto_history_metadata),
+        populate_redis_queue.partial(queue_name=FOREX_HISTORY_QUEUE).expand(data=forex_history_metadata),
+        populate_redis_queue.partial(queue_name=FUTURES_HISTORY_QUEUE).expand(data=futures_history_metadata),
+        populate_redis_queue.partial(queue_name=INDICES_HISTORY_QUEUE).expand(data=indices_history_metadata),
+        populate_redis_queue(queue_name=FILES_QUEUE, data=extracted_metadata)
+    ]
 
-    # ==========================
-    # EVENTS EXTRACTION TASKS
-    # ==========================
-    file_path = download_file(WORLDWIDE_EVENTS_CSV_FILE_URL, SHARED_FOLDER_PATH_AIRFLOW)
-    extracted_path = unzip_file(file_path, SHARED_FOLDER_PATH_AIRFLOW)
-    populate_redis_queue(extracted_path, "files_queue") >> end_task
-
-    # ==========================
     # TASK DEPENDENCIES
-    # ==========================
-
     start() >> [ 
         crypto_symbols,
         forex_symbols,
@@ -275,6 +275,6 @@ def ingestion_pipeline():
         indices_symbols, 
         file_path
         ]
-
+    populate_tasks >> end()
 
 ingestion_pipeline()
