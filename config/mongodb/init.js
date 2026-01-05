faf972b00cc2d254e7a86b5f4eb1bb261f8a54f4
Adrian <adriansaleh061@gmail.com>
2026-01-05 15:57:47 +0100

    removed test configs

--- config/mongodb/init.js
+++ config/mongodb/init.js
@@ -1,21 +1,12 @@
 // Variables
-const dbName = "stocks";
+const dbName = "raw_data_db";
 
 // Creation of the db
 db = db.getSiblingDB(dbName);
 
-db.createCollection("stock_prices")
-db.createCollection("companies_info")
-db.createCollection("stock_transactions")
-db.createCollection("crypto_prices")
-db.createCollection("crypto_transactions")
-db.createCollection("political_events")
-db.createCollection("politicians")
-db.createCollection("politicians_market_transactions")
-
+db.createCollection("ingestion"); 
 
 // Creation of CRUD user in the stocks database
-// TODO check env variables for user and password
 db.createUser({
   user: "crud",
   pwd: "crud",
--- config/postgres/init.sql
+++ config/postgres/init.sql
@@ -1,106 +1,175 @@
-CREATE TABLE Continent (
-    Id INT PRIMARY KEY,
-    ContinentName VARCHAR(100) NOT NULL, 
-    ContinentCode VARCHAR(10) UNIQUE NOT NULL
+-- Create schemas
+CREATE SCHEMA raw;
+CREATE SCHEMA staging;
+CREATE SCHEMA production;
+
+-- Create table
+CREATE TABLE raw.INDEX_EXCHANGE (
+    symbol VARCHAR(20)  PRIMARY KEY,
+    currency VARCHAR(4) ,
+    region VARCHAR(5) ,
+    index_name TEXT , -- Here it's the long name, ex S&P 500
+    exchange_timezone TEXT  -- Will be useful if we want to look at the "real" value of a company
 );
 
-CREATE TABLE Country (
-    Id INT PRIMARY KEY,
-    CountryName VARCHAR(100) NOT NULL,
-    CountryCode VARCHAR(10) UNIQUE NOT NULL,
-    ContinentId INT NOT NULL, 
-    FOREIGN KEY (ContinentId) REFERENCES Continent(Id)
+-- This is the value of the indes, it's a time series
+CREATE TABLE raw.INDEX_HISTORY (
+    symbol VARCHAR(20) ,
+    value_timestamp Varchar(50) ,
+    value_open DECIMAL(30, 2) ,
+    value_high DECIMAL(30, 2) ,
+    value_low DECIMAL(30, 2) ,
+    value_close DECIMAL(30, 2) ,
+    volume BIGINT ,
+    dividends DECIMAL(30, 2) ,
+    stock_splits INT ,
+    PRIMARY KEY (symbol, value_timestamp),
+    CONSTRAINT fk_index
+        FOREIGN KEY (symbol) REFERENCES raw.INDEX_EXCHANGE(symbol)
 );
 
-CREATE TABLE Holder (
-    Id INT PRIMARY KEY
+-- [ Forex ]
+
+-- Infos of forex exchange
+CREATE TABLE raw.FOREX_EXCHANGE (
+    symbol VARCHAR(20)  PRIMARY KEY,
+    forex_name VARCHAR(41) , -- This is for example "EUR/USD"
+    region VARCHAR(5),
+    exchange_timezone TEXT 
 );
 
-CREATE TABLE Company (
-    Id INT PRIMARY KEY,
-    CompanyName VARCHAR(255) NOT NULL,
-    CompanyCode VARCHAR(50) UNIQUE NOT NULL,
-    CountryId INT,
-    HolderId INT UNIQUE,
-    FOREIGN KEY (CountryId) REFERENCES Country(Id),
-    FOREIGN KEY (HolderId) REFERENCES Holder(Id)
+-- Forex values in timeseries
+
+CREATE TABLE raw.FOREX_HISTORY (
+    symbol VARCHAR(20) ,
+    value_timestamp Varchar(50) ,
+    value_open DECIMAL(30, 2) ,
+    value_high DECIMAL(30, 2) ,
+    value_low DECIMAL(30, 2) ,
+    value_close DECIMAL(30, 2) ,
+    dividends DECIMAL(30,2) , -- This is only for loading -> will be deleted later
+    volume BIGINT ,
+    stock_splits INT ,
+    PRIMARY KEY (symbol, value_timestamp),
+    CONSTRAINT fk_forex 
+        FOREIGN KEY (symbol) REFERENCES raw.FOREX_EXCHANGE(symbol)
 );
 
-CREATE TABLE Individual (
-    Id INT PRIMARY KEY,
-    FirstName VARCHAR(100) NOT NULL,
-    LastName VARCHAR(100) NOT NULL,
-    Residency INT,
-    NetWorth DECIMAL(20,2),
-    HolderId INT UNIQUE,
-    FOREIGN KEY (HolderId) REFERENCES Holder(Id),
-    FOREIGN KEY (Residency) REFERENCES Country(Id)
+-- Futures infos
+-- Infos of forex exchange
+CREATE TABLE raw.FUTURES_EXCHANGE (
+    symbol VARCHAR(20)  PRIMARY KEY,
+    futures_name VARCHAR(41) ,
+    region VARCHAR(5),
+    currency VARCHAR(5),
+    exchange_timezone TEXT
 );
 
-CREATE TABLE Market (
-    Id INT PRIMARY KEY,
-    MarketName VARCHAR(100) NOT NULL,
-    MarketCode VARCHAR(10) UNIQUE NOT NULL,
-    Status VARCHAR(50) NOT NULL CHECK (Status IN ('Bearish', 'Bullish', 'Stable')),
-    CountryId INT NOT NULL,
-    FOREIGN KEY (CountryId) REFERENCES Country(Id)
+-- Futures values
+CREATE TABLE raw.FUTURES_HISTORY (
+    symbol VARCHAR(20) ,
+    value_timestamp Varchar(50) ,
+    value_open DECIMAL(30, 2) ,
+    value_high DECIMAL(30, 2) ,
+    value_low DECIMAL(30, 2) ,
+    value_close DECIMAL(30, 2) ,
+    dividends DECIMAL(30,2) , -- This is only for loading -> will be deleted later
+    volume BIGINT ,
+    stock_splits INT ,
+    PRIMARY KEY (symbol, value_timestamp),
+    CONSTRAINT fk_futures
+        FOREIGN KEY (symbol) REFERENCES raw.FUTURES_EXCHANGE(symbol)
 );
 
--- TODO
-CREATE TABLE Asset (
-    Id INT PRIMARY KEY,
-    AssetType VARCHAR(50) NOT NULL CHECK (AssetType IN ('Forex', 'Commodity', 'Stock', 'ETF', 'Cryptocurrency')),
-    Description TEXT,
-    Provider INT NOT NULL, 
-    Value DECIMAL(20,6) NOT NULL,
-    Market INT NOT NULL, 
-    FOREIGN KEY (Market) REFERENCES Market(Id),
-    FOREIGN KEY (Provider) REFERENCES Company(Id)
+
+-- Crypto infos
+
+CREATE TABLE raw.CRYPTO_EXCHANGE (
+    symbol VARCHAR(20)  PRIMARY KEY,
+    crypto_name VARCHAR(41) , -- This is for example "BTC-USD"
+    region VARCHAR(5),
+    exchange_timezone TEXT 
 );
 
-CREATE TABLE Transaction (
-    Id INT PRIMARY KEY,
-    HolderId INT,
-    AssetId INT,
-    TransactionType VARCHAR(50) NOT NULL CHECK (TransactionType IN ('Buy', 'Sell')),
-    Quantity DECIMAL(20,4) NOT NULL,
-    PricePerUnit DECIMAL(20,2) NOT NULL,
-    TransactionDate TIMESTAMP NOT NULL,
-    FOREIGN KEY (HolderId) REFERENCES Holder(Id),
-    FOREIGN KEY (AssetId) REFERENCES Asset(Id)
+-- Crypto values
+
+CREATE TABLE raw.CRYPTO_HISTORY (
+    symbol VARCHAR(20) ,
+    value_timestamp Varchar(50) ,
+    value_open DECIMAL(30, 2) ,
+    value_high DECIMAL(30, 2) ,
+    value_low DECIMAL(30, 2) ,
+    value_close DECIMAL(30, 2) ,
+    dividends DECIMAL(30,2) , -- This is only for loading -> will be deleted later
+    volume BIGINT ,
+    stock_splits INT ,
+    PRIMARY KEY (symbol, value_timestamp),
+    CONSTRAINT fk_crypto
+        FOREIGN KEY (symbol) REFERENCES raw.CRYPTO_EXCHANGE(symbol)
 );
 
-CREATE TABLE Event (
-    Id INT PRIMARY KEY,
-    EventType VARCHAR(100) NOT NULL CHECK (EventType IN ('Economic', 'Political', 'Natural Disaster', 'Technological', 'Social')),
-    Title VARCHAR(255) NOT NULL,
-    Description TEXT,
-    EventDate TIMESTAMP NOT NULL
+
+-- [ UCDP DATA ]
+CREATE TABLE raw.CONFLICT(
+    conflict_id INT , -- this is the real id of the conflict but, because this is repeated it would be a problem for PG
+    "location" TEXT ,
+    "year" VARCHAR(4) , -- This will be used with the id as the primary key
+
+    -- Actors (side a, b, 2nd a, 2nd b)
+    side_a TEXT ,
+    side_b TEXT ,
+    side_a_2nd TEXT,
+    side_b_2nd TEXT,
+
+    --  Actors IDs from GWNO
+    gwno_a TEXT ,
+    gwno_b TEXT ,
+    gwno_b_2nd TEXT,
+    gwno_a_2nd TEXT,
+
+    -- Other infos
+    region TEXT , 
+    intensity_level INT ,
+    cumulative_intensity INT ,
+    type_of_conflict INT ,
+    incompatibility INT ,
+    territory_name TEXT,
+
+
+    -- Dates
+    "start_date" DATE , -- conflict start date
+    "start_date2" DATE , -- episode start date
+    ep_end_date DATE,
+    PRIMARY KEY (conflict_id, "year")
 );
 
-CREATE TABLE CountryEvent (
-    CountryId INT,
-    EventId INT,
-    PRIMARY KEY (CountryId, EventId),
-    FOREIGN KEY (CountryId) REFERENCES Country(Id),
-    FOREIGN KEY (EventId) REFERENCES Event(Id)
+CREATE TABLE raw.UCDP_ACTORS(
+    actor_id INT,
+    actor_name TEXT,
+    actor_og_name TEXT,
+    conflict_ids TEXT,
+    org_level INT,
+    PRIMARY KEY (actor_id)
 );
 
-CREATE TABLE AssetEvent (
-    AssetId INT,
-    EventId INT,
-    Magnitude DECIMAL(10,2),
-    TypeImpact VARCHAR(50) CHECK (TypeImpact IN ('Positive', 'Negative', 'Neutral')),
-    Description TEXT,
-    PRIMARY KEY (AssetId, EventId),
-    FOREIGN KEY (AssetId) REFERENCES Asset(Id),
-    FOREIGN KEY (EventId) REFERENCES Event(Id)
+CREATE TABLE raw.UCDP_GEOREFERENCE(
+    "id" INT,
+    conflict_new_id INT,
+    country TEXT,
+    region TEXT,
+    PRIMARY KEY (id)
 );
 
-CREATE TABLE MarketEvent (
-    MarketId INT,
-    EventId INT,
-    PRIMARY KEY (MarketId, EventId),
-    FOREIGN KEY (MarketId) REFERENCES Market(Id),
-    FOREIGN KEY (EventId) REFERENCES Event(Id)
-);
+-- Permissions
+GRANT CONNECT ON DATABASE stock TO admin;
+
+GRANT USAGE, CREATE ON SCHEMA raw, staging, production TO admin;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA raw, staging, production 
+GRANT SELECT, INSERT, UPDATE, DELETE, TRUNCATE ON TABLES TO admin;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA raw, staging, production 
+GRANT USAGE, SELECT ON SEQUENCES TO admin;
+
+GRANT SELECT, INSERT, UPDATE, DELETE, TRUNCATE ON ALL TABLES IN SCHEMA raw, staging, production TO admin;
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA raw, staging, production TO admin;
